9a3e0167df34838dbba3c0d9f034dcc1da7de655
jh <pleasemrlostman@naver.com>
2023-09-27 15:23:07 +0900

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import routes from "@/routes";
-import { useState } from "react";
 const router = createBrowserRouter(routes);
 const App = () => {
-  const [test, setTest] = useState();
   return <RouterProvider router={router} />;
 };
 export default App;
