111ba1e2e8a83b58725badbf76d3c9a0f610befd
jh <pleasemrlostman@naver.com>
2023-11-15 20:57:36 +0900

    handleSubmit 함수 추가

--- src/common/components/textarea/example.tsx
+++ src/common/components/textarea/example.tsx
@@ -1,23 +1,24 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
 import { Textarea, TextareaWithController } from ".";
 import { DevTool } from "@hookform/devtools";
 const Example = () => {
-  const { control } = useForm({
+  const { control, handleSubmit } = useForm({
     defaultValues: {
       "text-area": "",
     },
   });
+
+  type SubmitType = {
+    "text-area": string;
+  };
+
+  const onSubmit = (data: SubmitType) => alert(`현재 값, ${data}`);
+  const onError = (error: FieldErrors<SubmitType>) => {
+    alert(`에러, ${error["text-area"]?.message}`);
+  };
+
   return (
-    <form
-    //   onSubmit={handleSubmit(
-    //     (data) => {
-    //       console.log("data", data);
-    //     },
-    //     (error) => {
-    //       console.log("error", error);
-    //     },
-    //   )}
-    >
+    <form onSubmit={handleSubmit(onSubmit, onError)}>
       <button>전송</button>
       <Textarea />
       <TextareaWithController
@@ -26,6 +27,10 @@ const Example = () => {
         name="text-area"
         rules={{
           required: "비어두지마",
+          pattern: {
+            message: "한글만 써라",
+            value: /^[가-힣]+$/,
+          },
         }}
       />
       <DevTool control={control} />
